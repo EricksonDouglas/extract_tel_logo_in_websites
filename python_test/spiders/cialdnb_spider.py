6a2ab60f658c1d111c3ee630e9fc9945d9e90d59
Erickson Matos <erickson.matos@omie.com.br>
2021-02-24 00:19:34 -0300

    feature/#4 Update name Class

--- python_test/spiders/cialdnb_spider.py
+++ python_test/spiders/cialdnb_spider.py
@@ -1,41 +1,34 @@
-import scrapy
+from scrapy import Spider, Request
 import python_test.utils.libregex as rust
-from sys import stdin, stdout
-from json import dumps as json_dumps
+from sys import stdin
 
 
 RE_PHONE: r'' = r"(((\d{1,4})|(\(\d{1,5}\)))(-| )((\d{1,5})|(\(\d{1,5}\)))(-| )(\d{1,5})(-\d{1,5}| \d{1,5}| )*.)</"
 RE_LOGO: r'' = r'"((\S*)(/|-|_)(\w*)(.png|.svg|.jpeg|.jpg))"'
 
 
-class cialdnbSpider(scrapy.Spider):
+class CialdnbSpider(Spider):
     name = "cialdnb"
 
     def start_requests(self) -> [{}]:
         urls: list = list(map(lambda x: x.replace('\n', ''), stdin.readlines()))
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield Request(url=url, callback=self.parse)
 
     def parse(self, response):
         _response: str = self.re_sub(response.text, pattern=r"<script..*</script>")
-        yield self._stdout_write({
+        yield {
             "website": response.url,
             "logo": self._checkout_logo(base=response.url,
                                         path=self.re_findall(_response, pattern=RE_LOGO, group=1)[0]),
             "phone": list(set(map(lambda x: self.re_sub(x, pattern=r"\D*"),
                                   self.re_findall(_response, pattern=RE_PHONE, group=1))))
-        })
+        }
 
     @staticmethod
     def _checkout_logo(base: str, path: str) -> str:
         return path if path.startswith("http") else \
             f"{'/'.join(base.split('/')[:3]) if base.count('/') > 2 else base}{path}"
-
-    @staticmethod
-    def _stdout_write(result: dict) -> dict:
-        stdout.write(json_dumps(result)+'\n')
-        stdout.flush()
-        return result
 
     @staticmethod
     def re_sub(text: str, *, pattern: r'' = r'\r|\n', repl: str = '') -> str:
